2c687597b3ab84bb011ace6cfaf14feff7b42b1e
Takanuma <58717371+Takanuma@users.noreply.github.com>
2020-01-12 22:07:33 +0900

    Update basic.js

--- basic.js
+++ basic.js
@@ -8,7 +8,7 @@ $(function(){
         if (url != '') {
             // フェードアウトしてから、取得したURLにリンクする
             $('#wrapper').fadeOut(500);
-            $('#wrapper').fadeOut(1000);
+            $('#wrapper').fadeIn(1000);
             setTimeout(function(){
                 location.href = url;
             }, 500);
